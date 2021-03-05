9a23044a8a322ec79aeb505f230be27c7ba13653
sky_pneuma <v.senkecich@intelico.su>
2021-03-05 11:23:49 +0300

    add bootstrap styles

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -19,13 +19,20 @@ const MainPage = () => {
   return (
     <div className="main-page">
       <Header />
-      <Row>
-        {countries.map((item, index) => (
-          <Col>
-            <Country />
-          </Col>
-        ))}
-      </Row>
+      <div className="container">
+        <div className="row">
+          <img
+            className="country_img"
+            src={`${process.env.PUBLIC_URL}./travelIcon.png`}
+            alt=""
+          />
+          {countries.map((item, index) => (
+            <div className="col-6">
+              <Country />
+            </div>
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   );
