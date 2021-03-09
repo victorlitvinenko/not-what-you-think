b5f490cfabc8facbb9e9bd50651e9187639fd497
sky_pneuma <v.senkecich@intelico.su>
2021-03-09 18:11:47 +0300

    refactor: delete bootstrap and rework country component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,12 @@
 import { Route, Switch } from 'react-router-dom';
-import Country from './pages/Country';
+import CountryPage from './pages/CountryPage';
 import MainPage from './pages/MainPage';
 
 const Routes = (): JSX.Element => (
   <Switch>
     <Route path="/" exact component={MainPage} />
     {/* <Route path="/country/:id" exact component={Country} /> */}
-    <Route path="/country/" exact component={Country} />
+    <Route path="/country/" exact component={CountryPage} />
   </Switch>
 );
 
---
+++ src/pages/CountryPage.tsx
@@ -0,0 +1,8 @@
+// import { useParams } from 'react-router-dom';
+import Country from '../components/Country/Country';
+
+const CountryPage: React.FC = () => {
+  // const { id } = useParams();
+  return <Country />;
+};
+export default CountryPage;
