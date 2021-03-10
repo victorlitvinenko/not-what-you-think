07159de6409fccee5486be353209442d59d1d6eb
sky_pneuma <v.senkecich@intelico.su>
2021-03-10 15:38:11 +0300

    feat: add campaign adaptive

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -7,82 +7,73 @@ import Time from '../Helpers/Time';
 const Country: React.FC = () => {
   return (
     <div className="country-box">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="country-title-box">
-              <div className="country-title-left">
-                <h3>Country name</h3>
-                <span>breadcrumbs</span>
-              </div>
-              <div className="country-title-right">
-                <Link to="/">
-                  <button type="button" onClick={() => {}}>
-                    Back to menu
-                  </button>
-                </Link>
-                <div className="df mt5">
-                  <Time />
-                  <span>weather</span>
-                </div>
-              </div>
+      <div className="row">
+        <div className="col">
+          <div className="country-title-box">
+            <div className="country-title-left">
+              <h3>Country name</h3>
+              <span>breadcrumbs</span>
+            </div>
+            <div className="country-title-right">
+              <Time />
+              <span>weather</span>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="row">
-          <div className="col">
-            <img className="country-img" src={img} alt="" />
-          </div>
-          <div className="col">
-            <div className="dfc">
-              <div className="j46 mb15">
-                <span>Description</span>
-                <a href="https://www.youtube.com/">
-                  Check video about this country
-                </a>
-              </div>
-              <span className="mb15">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </span>
-              <div>
-                <div className="row">
-                  <div className="col">
-                    <div className="dfc">
-                      <span>Capital</span>
-                      <span>Minsk</span>
-                    </div>
-                  </div>
-                  <div className="col">
-                    <div className="dfc">
-                      <span>Stars</span>
-                      <span>Star Star Star</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <img className="country-img" src={img} alt="" />
         </div>
-
-        <div className="row">
-          <div className="col">
+        <div className="col-12 col-md-6">
+          <div className="dfc">
+            <div className="j46 mb15">
+              <span>Description</span>
+              <a href="https://www.youtube.com/">
+                Check video about this country
+              </a>
+            </div>
+            <span className="mb15">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </span>
             <div>
-              <h5>Attraction</h5>
+              <div className="row">
+                <div className="col">
+                  <div className="dfc">
+                    <span>Capital</span>
+                    <span>Minsk</span>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="dfc">
+                    <span>Stars</span>
+                    <span>Star Star Star</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="row">
-          <div className="col">
-            <div>map</div>
+      <div className="row">
+        <div className="col-12">
+          <div className="mt25">
+            <h5>Attraction</h5>
           </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div>map</div>
         </div>
       </div>
     </div>
