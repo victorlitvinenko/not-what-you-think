50c2a7c27e7840eeed873b07e9de6b5b74cda3f5
Sergey Pozharitsky <pozhar511@gmail.com>
2021-03-11 17:23:48 +0300

    feat: add styles & flags, removed elements

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,31 +5,58 @@ const Footer: React.FC = () => {
     <footer className="footer">
       <div className="footer__content">
         <div>Created by </div>
-        <a href="https://github.com/victorlitvinenko/" className="rollover__vl">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/victorlitvinenko/"
+          className="rollover__vl"
+        >
           Victor
         </a>
-        <a href="https://github.com/goonext/" className="rollover__vd">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/goonext/"
+          className="rollover__vd"
+        >
           Valeriy
         </a>
-        <a href="https://github.com/cqxg/" className="rollover__ii">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/cqxg/"
+          className="rollover__ii"
+        >
           Ihar
         </a>
-        <a href="https://github.com/skypneuma" className="rollover__vs">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/skypneuma"
+          className="rollover__vs"
+        >
           Vyacheslav
         </a>
         <a
+          target="_blank"
+          rel="noreferrer"
           href="https://github.com/vladimirleontev281"
           className="rollover__vld"
         >
           Vladimir
         </a>
-        <a href="https://github.com/sergeytestweb/" className="rollover__sp">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/sergeytestweb/"
+          className="rollover__sp"
+        >
           Sergey
         </a>
         <div> in 2k21</div>
       </div>
       <div>
-        <a href="https://rs.school/js/">
+        <a target="_blank" rel="noreferrer" href="https://rs.school/js/">
           <img
             width="55"
             src="https://rs.school/images/rs_school_js.svg"
