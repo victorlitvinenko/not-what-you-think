0c41c7655bdb159fad129615cd6a982598edf2f1
Valera <v.drozdovich@intelico.su>
2021-03-12 13:12:20 +0300

    feat: add map for country

--- src/components/Helpers/Map.tsx
+++ src/components/Helpers/Map.tsx
@@ -1,17 +1,55 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet';
+import L from 'leaflet';
+
+import iconUrl from '../../assets/gif/mapgif.gif';
+
+const LocationMarker = () => {
+  const [position, setPosition] = useState({
+    lat: 53.902284,
+    lng: 27.561831,
+  });
+
+  const iconPerson = new L.Icon({
+    iconUrl,
+    iconSize: new L.Point(80, 85),
+  });
+
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker icon={iconPerson} position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+};
 
 const Map: React.FC = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={{ lat: 53.902284, lng: 27.561831 }}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution="Created by top 1 team in the world"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LocationMarker />
     </MapContainer>
   );
 };
