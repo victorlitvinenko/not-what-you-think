d28fc63f58fa45fd549686e6124f71f655a3d40c
Valeriy Drozdovich <valeracool133@gmail.com>
2021-03-13 22:28:33 +0300

    fix: inputSearch.tsx fix

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Form, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CountryStore from '../../stores/country-store';
@@ -11,6 +11,7 @@ const InputSearch: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState<CountryType[]>([]);
   const [outClicked, setOutClicked] = useState(false);
+  const target = useRef(null);
 
   useEffect(() => {
     const results: CountryType[] = CountryStore.countries.filter((country) =>
@@ -20,19 +21,22 @@ const InputSearch: React.FC = () => {
   }, [searchTerm]);
 
   return (
-    <Form.Group style={{ position: 'relative' }} className="ml-3">
+    <Form.Group ref={target} style={{ position: 'relative' }} className="ml-3">
       <FormControl
         type="text"
         placeholder="Search country"
         onFocus={() => setOutClicked(false)}
-        onBlur={() => setOutClicked(true)}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       {searchTerm.length && !outClicked ? (
         <div className={styles.inputsearch} style={{ position: 'absolute' }}>
           {searchResults.map((item: CountryType) => {
             return (
-              <Link key={item._id} to={`/country/${item._id}`}>
+              <Link
+                onClick={() => setOutClicked(true)}
+                key={item._id}
+                to={`/country/${item._id}`}
+              >
                 <div>{item.name}</div>
               </Link>
             );
